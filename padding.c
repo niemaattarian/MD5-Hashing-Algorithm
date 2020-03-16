5d32d507f2785518b2e1ec5a0479bb280a583f54
niemaattarian <G00346901@gmit.ie>
2020-03-16 18:26:37 +0000

    More padding

--- padding.c
+++ padding.c
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+uint64_t nozerobytes(uint64_t nobits){
+
+    uint64_t results = 512ULL - (nobits % 512ULL);
+
+    if (results < 65){
+        results += 512;
+    }
+    results -= 72;
+    
+    return (results / 8ULL);
+}
+
 int main(int argc, char * argv[]){
 
     if(argc != 2){
@@ -20,13 +32,20 @@ int main(int argc, char * argv[]){
     uint8_t b;
     uint64_t nobits;
 
-    for (nobits = 0;fread(&b, 1, 1, infile) == 1; nobits += 8){
+    for (nobits = 0; fread(&b, 1, 1, infile) == 1; nobits += 8){
         // Format specifier
-        printf("%02" PRIx8 " ", b);
+        printf("%02" PRIx8, b);
     }
+
+    printf("%02" PRIx8, 0x80); // Bits: 1000 0000
+
+    for(uint64_t i = nozerobytes(nobits); i > 0; i--){
+        printf("%02" PRIx8, 0x00);
+    }
+
     // Format specifier
     printf("%016" PRIx64 "\n", nobits);
-    
+
     printf("\n");
 
     // Close file
