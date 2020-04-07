ddd24d97ea6b04cdfad2afc1a1bfed8525fc657b
niemaattarian <G00346901@gmit.ie>
2020-04-08 00:49:45 +0100

    Can open file

--- MD5/main.c
+++ MD5/main.c
@@ -1,8 +1,21 @@
+// Niema Attarian - MD5 Hashing Algorithm
 #include <stdio.h>
+#include <inttypes.h>
 
-int main()
-{
-	printf("Test\n");
+int main(int argc, char *argv[]) {
 
-	return 0;
+    if (argc != 2){
+        printf("Error: expected single filename as arguemnt.\n");
+        return 1;
+    }
+
+    FILE *infile = fopen(argv[1], "rb");
+    if(!infile) {
+        printf("Error: couldn't open file %s. \n", argv[1]);
+        return 1;
+    }
+
+    fclose(infile);
+
+    return 0;
 }
